f251f70eabd5dac25ab78ad5225d2a65f524b71e
Israel Monteiro <israelmonteiro.dev@gmail.com>
2026-08-03 16:09:13 -0300

    feat: add footer component

---
+++ src/components/MenuItems/index.tsx
@@ -0,0 +1,29 @@
+const menus = [
+    { title: "Masculino", items: ["Casual", "Esporte", "Moderno", "Futurista"] },
+    { title: "Feminino", items: ["Casual", "Esporte", "Moderno", "Futurista"] },
+    { title: "Outlet", items: ["Masculino", "Feminino"] },
+    { title: "Sobre", items: ["Quem Somos", "Missão"] },
+];
+
+export const MenuItems = () => {
+    return (
+        <div className="flex flex-col sm:flex-row gap-8">
+            {menus.map(({ title, items }) => (
+                <nav  key={title}>
+                    <ul className="flex flex-col gap-4">
+                        <li>
+                            <p className="font-normal text-surface-alt text-xl">{title}</p>
+                        </li>
+                        {items.map((item) => (
+                            <li key={item}>
+                                <a href="#" className="font-medium hover:text-[#cccccc] transition-colors text-xl">
+                                    {item}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            ))}
+        </div>
+    );
+};
---
+++ src/components/SubscriptionForm/index.tsx
@@ -0,0 +1,14 @@
+export const SubscriptionForm = () => {
+    return (
+        <form className="flex flex-col">
+            <label htmlFor="newsletter">Inscreva-se em nosso e-mail</label>
+            <input
+                type="email"
+                id="newsletter"
+                name="newsletter"
+                placeholder="Email@email.com"
+                className="rounded-[30px] bg-white py-3 px-5 placeholder-[#aaaaaa] text-gray-600"
+            />
+        </form>
+    );
+};
