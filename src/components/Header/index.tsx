a0dcd6120db96bbef898ccdd5738032aee01cca8
Israel Monteiro <israelmonteiro.dev@gmail.com>
2026-08-29 22:02:16 -0300

    feat: add mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,18 @@ import IconUser from "@/assets/images/icons/icon-user.svg";
 import IconAbout from "@/assets/images/icons/icon-about.svg";
 import { Link } from "@tanstack/react-router";
 import { ShoppingCart } from "../ShoppingCart";
+import { MenuMobile } from "../MenuMobile";
+
+export interface NavLink {
+    name: string;
+    href: string;
+}
+
+const navLinks: NavLink[] = [
+    { name: "Masculino", href: "/products" },
+    { name: "Feminino", href: "/products" },
+    { name: "Outlet", href: "/products" },
+];
 
 export const Header = () => {
     return (
@@ -13,43 +25,39 @@ export const Header = () => {
                         <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
                     </Link>
 
-                    <nav className="hidden md:block">
+                    <nav className="hidden lg:block">
                         <ul className="flex gap-10">
-                            <li>
-                                <a href="#">Masculino</a>
-                            </li>
-                            <li>
-                                <a href="#">Feminino</a>
-                            </li>
-                            <li>
-                                <a href="#">Outlet</a>
-                            </li>
+                            {navLinks.map((link) => (
+                                <Link to={link.href} key={link.name}>
+                                    {link.name}
+                                </Link>
+                            ))}
                         </ul>
                     </nav>
 
                     <nav>
-                        <ul className="flex gap-4 md:gap-10">
-                            <li className="hidden md:block">
+                        <ul className="flex gap-4 md:gap-10 items-center">
+                            <li className="hidden lg:block">
                                 <Link to="/our-stores">Nossas Lojas</Link>
                             </li>
-                            <li className="hidden md:block">
+                            <li className="hidden lg:block">
                                 <Link to="/about">Sobre</Link>
                             </li>
-                            <li>
+                            <li className="lg:hidden">
+                                <MenuMobile navLinks={navLinks} />
+                            </li>
+                            <li className="hidden lg:block">
                                 <a href="#">
                                     <img src={IconUser} alt="Icone de login" />
                                 </a>
                             </li>
-                            <li>
-                                <a href="#">
+                            <li className="hidden lg:block">
+                                <Link to="/about">
                                     <img src={IconAbout} alt="Icone de sobre" />
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                {/* <a href="#">
-                                    <img src={IconCart} alt="Icone de carrinho" />
-                                </a> */}
-                                <ShoppingCart/>
+                                <ShoppingCart />
                             </li>
                         </ul>
                     </nav>
